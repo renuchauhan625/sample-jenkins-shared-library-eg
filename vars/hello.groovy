f33f56b34ea90c67feb833e03b35c5209f3f8b7f
Renu Chauhan <61846916+renuchauhan625@users.noreply.github.com>
2022-09-07 15:50:49 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'default',String=dayOfWeek) {
+def call(String name = 'default',String dayOfWeek) {
   echo "Hello, ${name}. Today is ${dayOfWeek}"
 }
