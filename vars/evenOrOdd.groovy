3544dcef7bb3f0622b19ae1784b9e0905e96541f
Renu Chauhan <61846916+renuchauhan625@users.noreply.github.com>
2022-09-08 12:04:25 +0530

    Create evenOrOdd.groovy

---
+++ vars/evenOrOdd.groovy
@@ -0,0 +1,27 @@
+def call(int buildNumber){
+    if (buildNumber%2==0){
+        pipeline{
+            agent any
+            stages{
+                stage("even number"){
+                    steps{
+                        echo "the Buildnumber is even"
+                    }
+                }
+            }
+        }
+    }
+else{
+    pipeline{
+        agent any
+        stages{
+            stage("odd number"){
+                steps{
+                    "The Build number is Odd"
+                }
+            }
+        }
+        
+    }
+}
+}
